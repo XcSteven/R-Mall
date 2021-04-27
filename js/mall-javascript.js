f1e0633aab47e0747c8eb227112af02e76e38594
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-04-27 14:46:42 +0700

    Add js file

---
+++ js/mall-javascript.js
@@ -0,0 +1,6 @@
+function StoreownerCheck() {
+	if (document.getElementById('storeown').checked) {
+		document.getElementById('ifStoreown').style.display = 'block';
+	}
+	else document.getElementById('ifStoreown').style.display = 'none';
+}

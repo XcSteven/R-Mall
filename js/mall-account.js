f70481b854e1edd93b64d090a95351299900079c
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-05-06 00:44:24 +0700

    Add and update js files

---
+++ js/mall-account.js
@@ -0,0 +1,67 @@
+function StoreownerCheck() {
+	if (document.getElementById('storeown').checked) {
+		document.getElementById('ifStoreown').style.display = 'block';}
+	else document.getElementById('ifStoreown').style.display = 'none';
+}
+
+function SamePassword() {
+	if (document.getElementById('pass').value == document.getElementById('rpass').value) {
+		document.getElementById('message').style.color = '#008000';
+		document.getElementById('message').innerHTML = 'matching';}
+	else {
+    document.getElementById('message').style.color = '#ff0000';
+    document.getElementById('message').innerHTML = 'not matching';}
+}
+
+function GetEmail()
+{
+	email = document.getElementById('email').value;
+	localStorage.setItem('InputValue', email);
+}
+	document.getElementById('UserEmail').innerHTML=localStorage.getItem('InputValue');
+
+function is_numeric(str) {
+	return /^\d+$/.test(str);
+}
+
+function valid_phone(phone) {
+	debugger;
+	phone_length = phone.length;
+	valid_characters = ['.', '-', ' '];
+	if (!is_numeric(phone[0]) | !is_numeric(phone[phone_length - 1])) {
+		console.log('First or last character invalid');
+		return false;
+    }
+	count_number = 0;
+	for (i = 0; i < phone_length; ++i) {
+		if (is_numeric(phone[i])) {
+			count_number++;
+		} else if (valid_characters.includes(phone[i])) {
+			if (!is_numeric(phone[i - 1]) | !is_numeric(phone[i + 1])) {
+				console.log('Two characters next each other');
+				return false;
+			}
+		} else {
+			console.log('Invalid characters');
+			return false;
+		}
+	}
+	if (count_number < 9 | count_number > 11) {
+		console.log('Length not satisfied');
+		return false;
+	}
+	return true;
+}
+
+function custom_valid() {
+	phonenum = document.getElementById('phone');
+	pv = valid_phone(phonenum.value);
+	if (!pv) {
+        phonenum.setCustomValidity('Phone number is invalid!');
+        phonenum.reportValidity();
+        phonenum.focus();
+        console.log('Phone is invalid');
+        return false;
+    }
+	return true;
+}
--- js/mall-contact.js
+++ js/mall-contact.js
@@ -1,6 +1,6 @@
 function checkbox_daycheck() {
 	flag = false;
-	a = document.getElementsByClassName("day_check");
+	a = document.getElementsByClassName('day_check');
 	Array.from(a).forEach(function(ele) {
 		console.log(ele, ele.checked);
 		if (ele.checked) {
@@ -19,7 +19,7 @@ function valid_phone(phone) {
 	phone_length = phone.length;
 	valid_characters = ['.', '-', ' '];
 	if (!is_numeric(phone[0]) | !is_numeric(phone[phone_length - 1])) {
-		console.log("First or last character invalid");
+		console.log('First or last character invalid');
 		return false;
     }
 	count_number = 0;
@@ -28,61 +28,61 @@ function valid_phone(phone) {
 			count_number++;
 		} else if (valid_characters.includes(phone[i])) {
 			if (!is_numeric(phone[i - 1]) | !is_numeric(phone[i + 1])) {
-				console.log("Two characters next each other");
+				console.log('Two characters next each other');
 				return false;
 			}
 		} else {
-			console.log("Invalid characters");
+			console.log('Invalid characters');
 			return false;
 		}
 	}
 	if (count_number < 9 | count_number > 11) {
-		console.log("Length not satisfied");
+		console.log('Length not satisfied');
 		return false;
 	}
 	return true;
 }
 
 function custom_valid() {
-	phone = document.getElementById("phone");
+	phone = document.getElementById('phone');
 	pv = valid_phone(phone.value);
 	if (!pv) {
-        phone.setCustomValidity("Phone number is invalid!");
+        phone.setCustomValidity('Phone number is invalid!');
         phone.reportValidity();
         phone.focus();
-        console.log("Phone is invalid");
+        console.log('Phone is invalid');
         return false;
     }
     if (!checkbox_daycheck()) {
-		document.getElementById("form_submit_result").innerHTML = "* At least one day must be checked!";
-		console.log("Check box must be checked");
-		document.getElementById("form_submit_result").focus();
+		document.getElementById('form_submit_result').innerHTML = '* At least one day must be checked!';
+		console.log('Check box must be checked');
+		document.getElementById('form_submit_result').focus();
 		return false;
 	}
 	return true;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-	fb_mess = document.getElementById("mess");
-	fb_mess.addEventListener("input", function() {
+document.addEventListener('DOMContentLoaded', function() {
+	fb_mess = document.getElementById('mess');
+	fb_mess.addEventListener('input', function() {
 		ml = fb_mess.value.length;
 		flag = false;
 		if (ml < 50) {
-			document.getElementById("letters").innerHTML = (50 - ml) + " more letters are needed";
+			document.getElementById('letters').innerHTML = (50 - ml) + ' more letters are needed';
 			if (!flag) {
-				fb_mess.classList.add("error-border");
+				fb_mess.classList.add('error-border');
 				flag = true;
 			}
 		} else if (ml > 500) {
-			document.getElementById("letters").innerHTML = "Deleting " + (ml - 500) + " more letters is needed";
+			document.getElementById('letters').innerHTML = 'Deleting ' + (ml - 500) + ' more letters is needed';
 			if (!flag) {
-				fb_mess.classList.add("error-border");
+				fb_mess.classList.add('error-border');
 				flag = true;
 			}
 		} else {
-			document.getElementById("letters").innerHTML = "You can type " + (500 - ml) + " more letters";
+			document.getElementById('letters').innerHTML = 'You can type ' + (500 - ml) + ' more letters';
 			flag = false;
-			fb_mess.classList.remove("error-border");
+			fb_mess.classList.remove('error-border');
 		}
 	})
 })

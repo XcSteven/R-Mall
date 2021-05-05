aa642ed15acc5822db59d082c6d960dac38f4cf1
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-05-06 00:43:46 +0700

    Delete mall-myaccount.js

--- js/mall-myaccount.js
+++
@@ -1,7 +0,0 @@
-function getData()
-{
-    var email = document.getElementById("email").value;
-    localStorage.setItem("InputValue", email);   
-}
-
-document.getElementById("UserEmail").innerHTML=localStorage.getItem("InputValue");

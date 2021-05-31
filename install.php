3bd9711ed6a267098731d525fa03fa342330050c
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-05-31 13:37:12 +0700

    Update install.php

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 	$config_dir =__DIR__.'/../../config.csv';
 	if(file_exists($config_dir)){
-    echo 'Your site had been configuratured';
+    echo 'Your site had been configurated';
 	} else {
 		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			if($_POST['username'] != "" & $_POST['password'] != ""){
@@ -22,16 +22,31 @@
 			}
 		} else {
 ?>
-<form method="post" action="">
-	<label>Username</label>
-	<input name="username" placeholder="Username" type="text"><br>
-	<label>Password</label>
-	<input name="password" type="password"><br>
-	<label>Retype password</label>
-	<input name="password" type="password">
-	<button type="submit">Submit</button>
-	<button type="reset">Reset</button>
-</form>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Install</title>
+		<link rel="stylesheet" href="css/mall-style.css">
+		<link rel="shortcut icon" href="images/favicon.ico">
+	</head>
+	<body>
+		<body style="background-color:#f5f5dc">
+		<div class="login">
+			<form method="post" action="">
+				<label>Username</label>
+				<input name="username" placeholder="Username" type="text"><br>
+				<label>Password</label>
+				<input name="password" type="password"><br>
+				<label>Retype password</label>
+				<input name="password" type="password">
+				<input type="submit" value="Submit">
+				<input type="reset" value="Reset">
+			</form>
+		</div>
+	</body>
+</html>
 <?php
 	}
 	}
